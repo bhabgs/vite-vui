a2280dad7c1a62f4f9c292cd1e59df348b33661c
bhabgs <bhabgs@aliyun.com>
2021-03-22 18:12:52 +0800

    feat(props自定义): 更新layout组件自定义宽高
    1. 新增对 footer header 的高度控制
    2. 新增对sider 组件的 宽度控制

--- package/vitevui/src/layout/lay_header.tsx
+++ package/vitevui/src/layout/lay_header.tsx
@@ -4,17 +4,25 @@
  * @Author: bhabgs
  * @Date: 2021-02-21 15:44:28
  * @LastEditors: bhabgs
- * @LastEditTime: 2021-02-23 10:50:33
+ * @LastEditTime: 2021-03-22 15:49:03
  */
 import { App, defineComponent } from 'vue';
 
 const viLayoutHeader = defineComponent({
   name: 'viLayoutHeader',
+  props: {
+    height: {
+      type: String,
+      default() {
+        return '64px';
+      },
+    },
+  },
   render() {
     const { $slots } = this;
     const def = $slots.default;
     return (
-      <section class='vite-layout-header'>
+      <section class='vite-layout-header' style={{ height: this.height }}>
         <def />
       </section>
     );
