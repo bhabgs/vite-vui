c4c01a1f7459cefeeccd43e0c52d7b175ffed3b2
bhabgs <bhabgs@aliyun.com>
2021-03-22 18:13:31 +0800

    chore(xxx): aaa

--- src/lay.tsx
+++ src/lay.tsx
@@ -4,10 +4,14 @@
  * @Author: bhabgs
  * @Date: 2021-02-21 15:42:58
  * @LastEditors: bhabgs
- * @LastEditTime: 2021-02-26 13:42:32
+ * @LastEditTime: 2021-03-19 13:27:18
  */
 import { defineComponent, reactive } from 'vue';
-import { RouterView } from 'vue-router';
+
+const menus = [
+  { name: 'deng', key: 'deng' },
+  { name: 'deng1', key: 'deng1' },
+];
 
 const state = reactive({
   t: 'ooo',
@@ -15,16 +19,23 @@ const state = reactive({
 
 export default defineComponent({
   components: {},
-  created() {
-    console.log(this.$vitevuu);
-  },
+  created() {},
   render() {
     return (
       <vi-layout>
         <vi-layout-header>header</vi-layout-header>
         <vi-layout>
           <vi-layout-sider>sider</vi-layout-sider>
-          <vi-layout-main>{state.t}</vi-layout-main>
+          <vi-layout-main
+            vContextmenu={{
+              menus,
+              callBack(e: any) {
+                console.log(e);
+              },
+            }}
+          >
+            {state.t}
+          </vi-layout-main>
         </vi-layout>
         <vi-layout-footer>footer</vi-layout-footer>
       </vi-layout>
