7dc9b1c2188231897accddd447d1cbb18f37c17a
bhabgs <bhabgs@aliyun.com>
2021-04-12 11:48:39 +0800

    feat(xxx): 工具外层变更

--- src/registered.ts
+++ src/registered.ts
@@ -4,11 +4,13 @@
  * @Author: bhabgs
  * @Date: 2020-11-30 09:53:41
  * @LastEditors: bhabgs
- * @LastEditTime: 2021-04-08 15:43:45
+ * @LastEditTime: 2021-04-09 15:43:50
  */
 import { App } from 'vue';
-import vui from '../package/vitevui/src/index';
-// import vui from '../package/vitevui';
+// import vui from '../package/vitevui/src/index';
+// import 'element-plus/lib/theme-chalk/index.css';
+import vui from '../package/vitevui';
+import '../package/vitevui/lib/style.css';
 import vitevuu from '../package/vitevuu';
 
 export default {
