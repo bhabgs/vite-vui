8a29c9bcb413ee92676c95771c051a5ef02ffdcc
孟子文 <mengziwen@tjmeiteng.com>
2021-08-31 14:36:37 +0800

    flowbug

--- src/components/test1.tsx
+++ src/components/test1.tsx
@@ -7,19 +7,13 @@
  * @LastEditTime: 2021-04-08 14:10:06
  */
 import { defineComponent, getCurrentInstance } from 'vue';
-import { getTabCurrentDetail } from '../../package/vitevui';
 
 export default defineComponent({
   setup() {
     const { proxy } = getCurrentInstance()!;
     const mitt = proxy!.$mitt;
     return () => (
-      <div
-        class='test'
-        onClick={() => {
-          console.log(getTabCurrentDetail('aaaa'));
-        }}
-      >
+      <div class='test' onClick={() => {}}>
         aaaa1
         <input type='text' />
       </div>
