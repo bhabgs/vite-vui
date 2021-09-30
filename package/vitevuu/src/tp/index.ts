47f884683a4e92918c245fef1d530efcb64b5edb
bhabgs <bhabgs@aliyun.com>
2021-09-30 10:51:48 +0800

    feat(tp): tp util

---
+++ package/vitevuu/src/tp/index.ts
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { DataModel, graph, List, deserializeCallBack } from 'ht';
+
+interface TPINIT {
+  callBack: deserializeCallBack | undefined;
+  json: string;
+  zoomable?: boolean;
+  pannable?: boolean;
+  dom?: unknown | HTMLDivElement;
+}
+
+export interface PROMISETP {
+  json: any;
+  dm: DataModel | null;
+  gv: graph.GraphView | null;
+  data: List | null;
+  reload: (url: string, cb: deserializeCallBack) => void;
+}
+
+export default (opt: TPINIT) => {
+  const dom = opt.dom as HTMLDivElement;
+  const graphView = new ht.graph.GraphView();
+  if (dom) {
+    graphView.addToDOM(dom);
+  }
+  return new Promise<PROMISETP>((res, rej) => {
+    graphView.deserialize(opt.json, (json, dm, gv, data) => {
+      if (json.title) document.title = json.title;
+      gv.setZoomable(opt.zoomable || false);
+      gv.setPannable(opt.pannable || false);
+      if (opt.callBack) opt.callBack(json, dm, gv, data);
+      res({
+        json,
+        dm,
+        gv,
+        data,
+        reload(url, cb) {
+          dm.clear();
+          gv.deserialize(url, cb);
+        },
+      });
+    });
+  });
+};
